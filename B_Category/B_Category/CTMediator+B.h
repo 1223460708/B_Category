7b6b3218e3c53522162097e80ab989fba2633e9d
1223460708 <1223460708@qq.com>
2018-03-05 10:32:59 +0800

    1

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,15 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by 炳神 on 2018/3/2.
+//  Copyright © 2018年 CBcc. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+@interface CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+@end
